a324929f6f6ec0de91da008282afe93e9cdaf252
Evgeniy Taranov <je.taranov@yandex.ru>
2023-09-06 01:25:04 +0300

    Правки после 2го ревью

--- app.js
+++ app.js
@@ -1,38 +1,48 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
+const { errors } = require('celebrate');
 const userRouter = require('./routes/users');
 const cardRouter = require('./routes/cards');
-const errorHandler = require('./middlewares/errorHandler');
 const NotFoundError = require('./errors/notFoundError');
 const auth = require('./middlewares/auth');
 const { login, createUser } = require('./controllers/users');
 
+const INTERNAL_SERVER_ERROR = 500;
 const { PORT = 3000 } = process.env;
 
 const app = express();
-
-mongoose.connect('mongodb://0.0.0.0:27017/mestodb', {
-  useNewUrlParser: true,
-});
 
 app.use(express.json());
 
 app.post('/signin', login);
 app.post('/signup', createUser);
 
-app.use(cookieParser());
-
 app.use(auth);
 
 app.use('/users', userRouter);
 app.use('/cards', cardRouter);
 
+app.use(cookieParser());
+
 app.use('*', (req, res, next) => {
   next(new NotFoundError('Неверный путь'));
 });
 
-app.use(errorHandler);
+app.use('/', errors());
+
+app.use((err, req, res, next) => {
+  if (err.statusCode) {
+    res.status(err.statusCode).send({ message: err.message });
+  } else {
+    res.status(INTERNAL_SERVER_ERROR).send({ message: 'Произошла ошибка' });
+  }
+  next();
+});
+
+mongoose.connect('mongodb://0.0.0.0:27017/mestodb', {
+  useNewUrlParser: true,
+});
 
 app.listen(PORT, () => {
 });
--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -1,24 +1,24 @@
-const UnauthorizedError = require('../errors/unauthorizedError');
-const NotFoundError = require('../errors/notFoundError');
-const BadRequestError = require('../errors/badRequestError');
-const ForbiddenError = require('../errors/forbiddenError');
+// const UnauthorizedError = require('../errors/unauthorizedError');
+// const NotFoundError = require('../errors/notFoundError');
+// const BadRequestError = require('../errors/badRequestError');
+// const ForbiddenError = require('../errors/forbiddenError');
 
-module.exports = (err, req, res) => {
-  if (err instanceof UnauthorizedError) {
-    res.status(err.statusCode).send({ message: err.message });
-  } else if (err instanceof NotFoundError) {
-    res.status(err.statusCode).send({ message: err.message });
-  } else if (err instanceof BadRequestError || err.name === 'ValidationError') {
-    res.status(400).send({ message: err.message });
-  } else if (err instanceof ForbiddenError) {
-    res.status(err.statusCode).send({ message: err.message });
-  } else if (err.name === 'CastError') {
-    res.status(400).send({ message: 'Некорректный формат ID' });
-  } else if (err.name === 'MongoError' && err.code === 11000) {
-    res.status(409).send({ message: 'Пользователь с таким email уже существует' });
-  } else if (err.name === 'JsonWebTokenError') {
-    res.status(401).send({ message: 'Некорректный токен' });
-  } else {
-    res.status(500).send({ message: 'Произошла ошибка на сервере' });
-  }
-};
+// module.exports = (err, req, res) => {
+//   if (err instanceof UnauthorizedError) {
+//     res.status(err.statusCode).send({ message: err.message });
+//   } else if (err instanceof NotFoundError) {
+//     res.status(err.statusCode).send({ message: err.message });
+//   } else if (err instanceof BadRequestError || err.name === 'ValidationError') {
+//     res.status(400).send({ message: err.message });
+//   } else if (err instanceof ForbiddenError) {
+//     res.status(err.statusCode).send({ message: err.message });
+//   } else if (err.name === 'CastError') {
+//     res.status(400).send({ message: 'Некорректный формат ID' });
+//   } else if (err.name === 'MongoError' && err.code === 11000) {
+//     res.status(409).send({ message: 'Пользователь с таким email уже существует' });
+//   } else if (err.name === 'JsonWebTokenError') {
+//     res.status(401).send({ message: 'Некорректный токен' });
+//   } else {
+//     res.status(500).send({ message: 'Произошла ошибка на сервере' });
+//   }
+// };
